1f9b64d669a9cbac25d1632332b59060727a6e38
Rupesh Kumbhar <rp.kumbhar2001@gmail.com>
2023-03-18 00:26:48 +0530

    footer socials added,contact page mobile ui

---
+++ src/Home/home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./../Home/home.css";
+
+function Home() {
+    return (
+        <div className="col-sm-12 p-0">
+            <div className="col-sm-8 p-0 mx-auto">
+                <h1 className=" mr-auto">
+                    Rupesh P Kumbhar
+                </h1>             
+            </div>
+            <h1 className="text-center my-5">
+                Home
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
