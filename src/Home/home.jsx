4f08666f034f69c1027471c72a20ff9365f5291a
Rupesh Kumbhar <rp.kumbhar2001@gmail.com>
2023-03-25 09:31:04 +0530

    Home page,name,css

--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -1,17 +1,48 @@
 import React from "react";
-import "./../Home/home.css";
+// import "./../Home/home.css";
+import "./../Home/home.scss";
 
 function Home() {
     return (
         <div className="col-sm-12 p-0">
             <div className="col-sm-8 p-0 mx-auto">
                 <h1 className=" mr-auto">
-                    Rupesh P Kumbhar
+                    Rupesh Kumbhar
                 </h1>             
+                <h1 className="text-center my-5">
+                    Home
+                </h1>
+
+            <div className="col-sm-12 p-0 row m-0 ">
+                <div className="col-sm-7 m-auto p-0">
+                    <div className="col-sm-8 m-auto">
+                    <div className="home-content-1 mb-2">
+                        Hello, I am
+                    </div>
+
+                    <div className="home-content-2 mb-2">
+                        Rupesh Kumbhar
+                    </div>
+                        <div class="typing-slider">
+                            <p>A Software Engineer</p>
+                            <p>Frontend Developer</p>
+                            <p>A Student</p>
+                    </div>
+
+                    </div>
+                </div>
+
+                <div className="col-sm-5 p-0">
+                    <div>
+                    <img
+                        src="assets/images/programming-man.svg"
+                        alt="image"
+                        className="home-img"
+                    />
+                    </div>
+                </div>
             </div>
-            <h1 className="text-center my-5">
-                Home
-            </h1>
+            </div>
         </div>
     )
 }
