4eaf0e0b2bab31d5fd7e505c8301ce3ff800f4cb
Rupesh Kumbhar <rp.kumbhar2001@gmail.com>
2024-02-28 22:24:31 +0530

    navbar react scroll css

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, Element } from 'react-scroll';
-import './../Navbar/navbar.css'
+import './../Navbar/navbar.scss'
 // import { Link, animateScroll as scroll } from 'react-scroll';
 
 
@@ -16,36 +16,37 @@ function Navbar(){
                     <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                         <ul class="navbar-nav ">
 
-                            <li class="nav-item">
+                            <li class="nav-item d-flex">
                                 <Link to="home" spy={true} smooth={true} offset={-100}
-                                    duration={500} class="nav-link " aria-current="page" href="#">Home</Link>
+                                    duration={500} class="nav-link navbar-link-css " aria-current="page" href="#" style={{ fontWeight: 600, fontSize: '20px', margin: 'auto', padding: '0.5rem' }} >Home</Link>
                             </li>
-
-
-                            <li class="nav-item">
-                                <Link to="skills" spy={true} smooth={true} offset={-100}
-                                    duration={500} class="nav-link " aria-current="page" href="#">Skills</Link>
-                            </li>
-
-                            <li class="nav-item">
+                            
+                            <li class="nav-item d-flex">
                                 <Link to="job-exp" spy={true} smooth={true} offset={-100}
-                                    duration={500} class="nav-link " aria-current="page" href="#">Job Experience</Link>
+                                    duration={500} class="nav-link navbar-link-css" aria-current="page" href="#" style={{ fontWeight: 600, fontSize: '20px', margin: 'auto', padding: '0.5rem' }} >Job Experience</Link>
                             </li>
 
-                            <li class="nav-item">
+                            <li class="nav-item d-flex">
+                                <Link to="skills" spy={true} smooth={true} offset={-100}
+                                    duration={500} class="nav-link navbar-link-css " aria-current="page" href="#" style={{ fontWeight: 600, fontSize: '20px' , margin: 'auto', padding: '0.5rem' }} >Skills</Link>
+                            </li>
+
+
+                            <li class="nav-item d-flex">
                                 <Link to="projects" spy={true} smooth={true} offset={-100}
-                                    duration={500} class="nav-link " aria-current="page" href="#">Projects</Link>
+                                    duration={500} class="nav-link navbar-link-css" aria-current="page" href="#" style={{ fontWeight: 600, fontSize: '20px', margin: 'auto', padding: '0.5rem' }} >Projects</Link>
                             </li>
 
-                            <li class="nav-item">
+                            <li class="nav-item d-flex">
                                 <Link to="internship" spy={true} smooth={true} offset={-100}
-                                    duration={500} class="nav-link " aria-current="page" href="#">Internship</Link>
+                                    duration={500} class="nav-link navbar-link-css" aria-current="page" href="#" style={{ fontWeight: 600, fontSize: '20px', margin: 'auto', padding: '0.5rem' }} >Internship</Link>
                             </li>
 
-                            <li class="nav-item">
+                            <li class="nav-item d-flex">
                                 <Link to="contact" spy={true} smooth={true} offset={-100}
-                                    duration={500} class="nav-link " aria-current="page" href="#">Contact</Link>
+                                    duration={500} class="nav-link navbar-link-css" aria-current="page" href="#" style={{ fontWeight: 600, fontSize: '20px', margin: 'auto', padding: '0.5rem' }} >Contact</Link>
                             </li>
+
 
                             {/* <li class="nav-item">
                                 <Link class="nav-link" to="home" smooth={true} duration={500}>Home</Link>
