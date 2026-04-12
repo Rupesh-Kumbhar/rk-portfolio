64d51224dc7f07b91853ec85c163070b10eaa4e5
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2026-04-12 09:56:42 +0530

    changing class to className - all projects

--- src/my-projects/angular-project/mean-pro.jsx
+++ src/my-projects/angular-project/mean-pro.jsx
@@ -15,7 +15,7 @@ function MeanPro(){
                                 <h5 className="card-title">Simple CRUD Operations using MEAN App</h5>
                                 <p className="card-text">This is Simple MEAN( MongoDB, ExpressJS, AngularJS, and NodeJS) App designed to Study how we can perform CRUD(Create, Read, Update, Delete) Operations. </p>
                                 <div className="col-sm-12 p-0 text-center">
-                                    <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#staticBackdropAngular">
+                                    <button type="button" className="btn btn-info" data-bs-toggle="modal" data-bs-target="#staticBackdropAngular">
                                         View More
                                     </button>
                                 </div>
@@ -26,14 +26,14 @@ function MeanPro(){
 
 
             {/* <!-- Modal --> */}
-            <div class="modal fade modal-bg-color " id="staticBackdropAngular" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h3 class="modal-title main-heading-pro" id="staticBackdropLabel">MEAN App CRUD Operations</h3>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal fade modal-bg-color " id="staticBackdropAngular" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h3 className="modal-title main-heading-pro" id="staticBackdropLabel">MEAN App CRUD Operations</h3>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             <div className="col-sm-12 p-0 row m-0">
                                 <h5 className="sub-heading-pro mb-4">
                                 Simple CRUD Operations using MEAN App :
@@ -49,25 +49,25 @@ function MeanPro(){
                                 <h5 className="sub-heading-pro mb-4">
                                     Angular, MongoDB, ExpressJS, NodeJS  HTML, CSS, Bootstrap, etc :
                                 </h5>
-                                <li className=" mb-4">
+                                <div className="mb-4">
                                     <b>Create : </b>
                                     Create involves Adding New data to the database. In a MEAN app, this can be achieved by sending a POST request from the front-end AngularJS application to a corresponding route in the ExpressJS framework, and saves it to the MongoDB database.
-                                </li>
+                                </div>
 
-                                <li className=" mb-4">
+                                <div className="mb-4">
                                     <b>Read : </b>
                                     Read involves Detching data . In a MEAN app, this can be achieved by GET request from the front-end to a corresponding route in the ExpressJS framework, and retrieves data from the MongoDB database and sends it back to the front-end application.
-                                </li>
+                                </div>
 
-                                <li className="mb-4">
+                                <div className="mb-4">
                                     <b>Update : </b>
                                     Update involves modifying existing data in the database. In a MEAN app, this is by sending a PUT request to a corresponding route in the ExpressJS framework, and then it gets updated, and saved back to the database.
-                                </li>
+                                </div>
 
-                                <li className="mb-4">
+                                <div className="mb-4">
                                     <b>Delete : </b>
                                     Delete involves removing data from the database. In a MEAN app, this is achieved by sending DELETE request to a corresponding route in the ExpressJS framework, and then it get deleted from the database.  
-                                </li>
+                                </div>
 
                             </div>
                             <div className="col-sm-5 p-0 row m-0 hide-in-mb">
@@ -76,16 +76,16 @@ function MeanPro(){
                             </div>
 
                         </div>
-                        <div class="modal-footer justify-content-evenly">
+                        <div className="modal-footer justify-content-evenly">
                             <div className="col-sm-12 p-0 row m-0 not-display-flex-mb">
                                 <div className="col p-0 row m-0 width-mb-100">
-                                    <button type="button" class="btn modal-btn w-auto mr-3">GitHub</button>
-                                    <button type="button" class="btn modal-btn w-auto mr-3">Live Link</button>
-                                    <button type="button" class="btn modal-btn w-auto">Demo Video</button>
+                                    <button type="button" className="btn modal-btn w-auto mr-3">GitHub</button>
+                                    <button type="button" className="btn modal-btn w-auto mr-3">Live Link</button>
+                                    <button type="button" className="btn modal-btn w-auto">Demo Video</button>
 
                                 </div>
                                 <div className="col p-0 row m-0 justify-content-end width-mb-100">
-                                    <button type="button" class="btn btn-secondary w-auto" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" className="btn btn-secondary w-auto" data-bs-dismiss="modal">Close</button>
                                 </div>
                             </div>
                         </div>
--- src/my-projects/college-projects/sy-project/sy-pro.jsx
+++ src/my-projects/college-projects/sy-project/sy-pro.jsx
@@ -19,7 +19,7 @@ function SyPro(){
                                 <h5 className="card-title">BMI Calculator</h5>
                                 <p className="card-text">This is BMI Calculator Mobile App in which we can calculate BMI of Person by giving Inputs like Gender, Height, Weight etc, We Developed it in SY of College . </p>
                                 <div className="col-sm-12 p-0 text-center">
-                                    <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#staticBackdropSy">
+                                    <button type="button" className="btn btn-info" data-bs-toggle="modal" data-bs-target="#staticBackdropSy">
                                         View More
                                     </button>
                                 </div>
@@ -30,14 +30,14 @@ function SyPro(){
 
 
             {/* <!-- Modal --> */}
-            <div class="modal fade modal-bg-color " id="staticBackdropSy" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h3 class="modal-title main-heading-pro" id="staticBackdropLabel">BMI Calculator</h3>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal fade modal-bg-color " id="staticBackdropSy" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h3 className="modal-title main-heading-pro" id="staticBackdropLabel">BMI Calculator</h3>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             <div className="col-sm-12 p-0 row m-0">
                                 <h5 className="sub-heading-pro mb-4">
                                 BMI Calculator :
@@ -53,26 +53,29 @@ function SyPro(){
                                 <h5 className="sub-heading-pro mb-4">
                                     Flutter, Dart, Google Cloud etc :
                                 </h5>
-                                <li className=" mb-4">
+                                <div className="mb-4">
                                     <b>Inputs : </b>
-                                    <li>Gender</li>
-                                    <li>Weight</li>
-                                    <li>Height</li>
-                                    <li>Age</li>
-                                </li>
+                                    <ul>
+                                        <li>Gender</li>
+                                        <li>Weight</li>
+                                        <li>Height</li>
+                                        <li>Age</li>
+                                    </ul>
+                                </div>
 
-                                <li className=" mb-4">
+                                <div className="mb-4">
                                     <b>Output : </b>
-                                    <li>BMI</li>    
-                                    <li>Workouts</li>
-                                    <li>Increase/Decrease Weight</li>
-                                    <li>Food Deit Plans</li>
-                                </li>
+                                    <ul>
+                                        <li>BMI</li>
+                                        <li>Workouts</li>
+                                        <li>Increase/Decrease Weight</li>
+                                        <li>Food Deit Plans</li>
+                                    </ul>
+                                </div>
 
-                                <li className="mb-4">
+                                <div className="mb-4">
                                     <b>Calculations/ References : </b>
-
-                                </li>
+                                </div>
 
                             </div>
                             <div className="col-sm-5 p-0 row m-0 hide-in-mb">
@@ -81,16 +84,16 @@ function SyPro(){
                             </div>
 
                         </div>
-                        <div class="modal-footer justify-content-evenly">
+                        <div className="modal-footer justify-content-evenly">
                             <div className="col-sm-12 p-0 row m-0 not-display-flex-mb">
                                 <div className="col p-0 row m-0 width-mb-100">
-                                    <button type="button" class="btn modal-btn w-auto mr-3">GitHub</button>
-                                    <button type="button" class="btn modal-btn w-auto mr-3">Live Link</button>
-                                    <button type="button" class="btn modal-btn w-auto">Demo Video</button>
+                                    <button type="button" className="btn modal-btn w-auto mr-3">GitHub</button>
+                                    <button type="button" className="btn modal-btn w-auto mr-3">Live Link</button>
+                                    <button type="button" className="btn modal-btn w-auto">Demo Video</button>
 
                                 </div>
                                 <div className="col p-0 row m-0 justify-content-end width-mb-100">
-                                    <button type="button" class="btn btn-secondary w-auto" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" className="btn btn-secondary w-auto" data-bs-dismiss="modal">Close</button>
                                 </div>
                             </div>
                         </div>
